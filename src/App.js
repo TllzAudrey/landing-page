0779e0157146b401078dd3c7505f4aba9d2c45e0
Audrey Telliez <audrey.telliez@estiam.com>
2025-01-22 18:37:54 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import profil from './audrey.jpg';
 import ImgMail from './mail.png';
@@ -10,25 +11,15 @@ import Image_droite from './files/Image_droite';
 import Image_gauche from './files/Image_gauche';
 
 function App() {
-  let titre_header = 'hiiiiiii ';
+  let titre_header = 'Portfolio ';
   let message_header ='Merci de visiter notre vaisseau spatial !';
-  let Champs_navbar= [
-    {name:'email', lien:'#apropos' },
-    {name:'email', lien:'text' },
-    {name:'email', lien:'text' }
+  let titre = 'Portfolio ';
+  let message ='Merci de visiter notre vaisseau spatial !';
 
-  ]
   return (
-    <>
-      <Header Champs_navbar={Champs_navbar} titre_header={titre_header} message_header={message_header} profil={profil} ImgMail={ImgMail} mail="telliez.a@gmail.com" ImgMap={ImgMap} map="Saint-Doulchard" ImgNumero={ImgNumero} num="06 28 18 79 92"/>
-
-      <Image_droite titre="Test" message="message_header" logo={logo} />
-      <Image_gauche titre={titre_header} message={message_header} logo={logo}  />
-      <Image_droite titre={titre_header} message={message_header} logo={logo} />
-
-      <Footer titre_footer={titre_header} message_footer={message_header} logo={logo} />
-    
-    </>
+  <>
+    <Header />
+  </>
    
   );
 }
